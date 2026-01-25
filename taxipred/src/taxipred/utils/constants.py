cd5d5500d21eb4fcdafd4c8d579a439e50867c4c
linus larsson <linus.anders.larsson@gmail.com>
2026-01-25 20:51:26 +0100

    I think api.py is done

--- taxipred/src/taxipred/utils/constants.py
+++ taxipred/src/taxipred/utils/constants.py
@@ -5,3 +5,18 @@ DATA_PATH = Path(__file__).parent.parent / "data"
 CLEANED_DATA_PATH = Path(__file__).parent.parent / "data" / "cleaned_data.csv"
 
 JOBLIB_PATH = Path(__file__).parent.parent / "models"
+
+MODEL_PATH = JOBLIB_PATH / "taxi_price_model.joblib"
+SCALER_PATH = JOBLIB_PATH / "taxi_price_scaler.joblib"
+
+SCALER_PATH = Path
+
+FEATURE_COLUMNS = [
+    "Trip_Distance_km",
+    "Trip_Duration_Minutes",
+    "Day_of_Week_Weekend",
+    "Traffic_Conditions_Low",
+    "Traffic_Conditions_Medium",
+    "Weather_Rain",
+    "Weather_Snow",
+]
